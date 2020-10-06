8c953825f6ab8d9d5e55ab091d4cfa2194673e60
JenisVaghasiya <72142142+JenisVaghasiya@users.noreply.github.com>
2020-10-06 18:44:38 +0530

    Update models.py
    changed

--- models.py
+++ models.py
@@ -5,8 +5,8 @@ class Contact(models.Model):
     name = models.CharField(max_length=122, null= True)
     email = models.CharField(max_length=122, null= True)
     phone = models.CharField(max_length=12, null= True)
-    desc = models.CharField(max_length=50, null=True)
-    date = models.DateField(auto_now_add=True,null= True)
+    desc = models.CharField(max_length=50)
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.name
